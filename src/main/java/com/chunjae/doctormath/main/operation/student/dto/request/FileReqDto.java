c71f66db78e0f82a54bdd7f8c03b7d05b885f9c3
NAMSEOYUN <synam32@chunjae.co.kr>
2022-11-23 16:06:32 +0900

    운영관리 파일 수정 2

---
+++ src/main/java/com/chunjae/doctormath/main/operation/student/dto/request/FileReqDto.java
@@ -0,0 +1,28 @@
+package com.chunjae.doctormath.main.operation.student.dto.request;
+
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class FileReqDto {
+
+    private String name;
+    private String curri;
+    private String grade;
+    private String userID;
+    private String parentID;
+    private String status;
+    private String shtell;
+    private String phtell;
+    private String sido;
+    private String gu;
+    private String school;
+    private String email;
+    private String addr1;
+    private String tell;
+    private String birth;
+    private String classDate;
+}
---
+++ src/main/java/com/chunjae/doctormath/main/operation/student/dto/request/SeqReqDto.java
@@ -0,0 +1,8 @@
+package com.chunjae.doctormath.main.operation.student.dto.request;
+
+import lombok.AllArgsConstructor;
+@AllArgsConstructor
+public class SeqReqDto {
+
+    private String key;
+}
